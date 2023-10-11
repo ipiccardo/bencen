35d8b54f20050c0fb6c25d96ecaead4fbec13c11
Julian Giudice <juliangiudice@hotmail.com>
2023-10-11 11:47:15 -0300

    created Service Card components - Part 2

---
+++ src/app/components/Service_Card/ServiceCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import classes from './serviceCard.module.css';
+
+interface Props {
+  className: any;
+  imageName: string;
+  header: string;
+  content: string;
+}
+
+export const ServiceCard = ({ imageName, header, content }: Props): JSX.Element => {
+  return (
+    <div className={classes.serviceCard}>
+      <div className={classes.group}>
+        <Image className={classes.image} alt="Excavator" src={`/icons/${imageName}.png`} width={96} height={96} />
+        <div className={classes.text}>
+          <div className={classes.header}>{header}</div>
+          <p className={classes.content}>{content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../styles/globals.css'
+import { ServiceCard } from '../components/Service_Card/ServiceCard'
 
 const HomePage = () => {
     return (
-        <div className='center'>
-            LA HOME
+        <div>
+            <div className='center'>
+                LA HOME
+            </div>
+            <ServiceCard className='civil' imageName='excavator' header='CIVIL WORKS' content='Excellence in construction, your trusted partner' />
         </div>
     )
 }
