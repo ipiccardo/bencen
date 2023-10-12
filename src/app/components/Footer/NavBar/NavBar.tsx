9725ac1e047c6415a12d0afc8c39228a4088daff
rodg22 <giudicerodrigo@hotmail.com>
2023-10-12 14:59:10 -0300

    Fix mobile footer

--- src/app/components/Footer/NavBar/NavBar.tsx
+++ src/app/components/Footer/NavBar/NavBar.tsx
@@ -15,21 +15,24 @@ const NavBar = ({ withHome, hasPipes, linkTitles = [] }: NavBarProps) => {
       <nav>
         <ul className={classes.navBar}>
           {withHome && (
-            <li>
+            <li className={classes.navBar__home}>
               <Link className={classes.navBar__link} href="/">
                 <Icon name="home" size={20} />
               </Link>
             </li>
           )}
-          {linkTitles.map((link: string, index: number) => {
+          {linkTitles.map((link: string, index: number, array: string[]) => {
             return (
               <li key={link + index}>
                 <Link
-                  className={classes.navBar__link}
+                  className={`font-16 font-w-500 ${classes.navBar__link}`}
                   href={`/dashboard/${convertLinkString(link)}`}
                 >
                   {link}
                 </Link>
+                {hasPipes && array.length - 1 !== index && (
+                  <span className={`${classes.navBar__pipes}`}> | </span>
+                )}
               </li>
             );
           })}
