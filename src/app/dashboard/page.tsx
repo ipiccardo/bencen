bfbe63fbe96e7253fedd398ba91d426cc801dc9f
Julian Giudice <juliangiudice@hotmail.com>
2023-10-14 15:59:06 -0300

    Created Unmatched Services Card - Part 1

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import '../styles/globals.css'
 import classes from './page.module.css';
-import SERVICES from '../utils/constants';
-import ServiceCard from '../components/Service_Card/ServiceCard'
+import ServiceCards from '../components/Service_Card/ServiceCard'
 import Footer from '../components/Footer/Footer';
+import UnmatchedCards from '../components/Unmatched_Card/UnmatchedCard';
 
 const HomePage = () => {
   return (
     <>
-      <ul className={classes.serviceCard}>
-        {Object.keys(SERVICES).map((serviceKey) => {
-          const [imageName, header, content] = SERVICES[serviceKey];
-          return (
-           <li key={serviceKey}>
-             <ServiceCard imageName={imageName} header={header} content={content} />
-           </li>
-           );
-        })}
-       </ul>
+      <ServiceCards />
+      <UnmatchedCards />
       <Footer />
     </>
   );
