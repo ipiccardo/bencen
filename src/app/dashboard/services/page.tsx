eedbbd46a3f884085a8d319c5f5e9cfd98b039a2
rodg22 <giudicerodrigo@hotmail.com>
2023-10-17 12:40:27 -0300

    Created branch for developing services section

--- src/app/dashboard/services/page.tsx
+++ src/app/dashboard/services/page.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import ServiceCards from "@/app/components/Service_Card/ServiceCard";
 
 const Services = () => {
-    return (
-        <div className='center'>SERVICES
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="center">INICIO SERVICES CON IMAGEN</div>
+      <ServiceCards />
+      <div>
+        <p>A LA IZQUIERDA</p>
+        <p>A LA DERECHA</p>
+        <p>A LA IZQUIERDA</p>
+        <p>A LA DERECHA</p>
+      </div>
+    </>
+  );
+};
 
-export default Services
+export default Services;
