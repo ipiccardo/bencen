a5c269fb99c7d5c3ce778bf5bafac6d430358e12
ipiccard <ivan.piccardo@tsoftlatam.com>
2023-11-15 13:01:05 -0300

    Armadas las primeras 3 secciones del home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,26 +10,28 @@ import classes from './page.module.css'
 import Header from "../components/Header/Header";
 import Image from "next/image";
 import Button from "../components/Ui/Button";
+import AboutUsCard from "../components/aboutUs_Card/AboutUsCard";
 
 
 const HomePage = () => {
 
-  const [pantallaPequeña, setPantallaPequeña] = useState(window.innerWidth < 768);
+  const [pantallaPequeña, setPantallaPequeña] = useState(false);
   const [firtAndLastImage, setFirstAndLastImage] = useState({ width: 1344, heigth: 888 })
   const [otherImages, setOtherImages] = useState({ width: 1296, heigth: 960 })
 
   const handleResize = () => {
+    setPantallaPequeña(window.innerWidth < 768)
     const screenWidth = window.innerWidth;
     console.log(screenWidth)
 
     if (screenWidth >= 1400) {
       setPantallaPequeña(false);
       setFirstAndLastImage({ width: 1344, heigth: 888 });
-      setOtherImages({ width: 1296, heigth: 960 })
+      setOtherImages({ width: 1344, heigth: 960 })
     } else if (screenWidth >= 941) {
       setPantallaPequeña(false);
       setFirstAndLastImage({ width: 938, heigth: 888 });
-      setOtherImages({ width: 900, heigth: 960 })
+      setOtherImages({ width: 938, heigth: 960 })
     } else {
       setPantallaPequeña(true);
     }
@@ -49,72 +51,92 @@ const HomePage = () => {
 
   return (
     <>
+      {/* <AboutUsCard /> */}
       <div className={classes.homePageContainer}>
         <div className={classes.headerContainer}>
           <Header />
         </div>
-        <section className={classes.firstSectionContainer}>
-          <div className={`${classes.imageContainer} ${classes.widthImage}`}>
-            {!pantallaPequeña &&
-              <Image width={firtAndLastImage.width} height={firtAndLastImage.heigth} src={"/images/insideBox.png"} alt={""} />
-            }
-          </div>
-        </section>
-        <div className={classes.serviceCardContainer}>
-          <ServiceCards />
-        </div>
-        <section className={classes.secondSectionContainer}>
-          <div className={`${classes.imageContainer} ${classes.secondImage}`}>
-            {!pantallaPequeña &&
-              <Image width={otherImages.width} height={otherImages.heigth} src={"/images/FrameTwoInside.png"} alt={""} />
-            }
-          </div>
-        </section>
-        <section className={classes.threeSectionContainer}>
-          <div className={`${classes.imageContainer} ${classes.otherImage}`}>
-            <div className={classes.containerAbsolute}>
-              <div className={classes.unmatchedServicesContainer}>
-                <h1>UNMATCHED SERVICES</h1>
-                <button>GET FREE QUOTATION</button>
-              </div>
-              <UnmatchedCards />
+        <div className={classes.firstSectionContainer}>
+          <section>
+            <div className={`${classes.FirstimageContainer} ${classes.widthImage}`}>
+              {!pantallaPequeña &&
+                <Image width={firtAndLastImage.width} height={firtAndLastImage.heigth} src={"/images/withoutMark.png"} alt={""} />
+              }
             </div>
-            {!pantallaPequeña &&
-              <Image width={otherImages.width} height={otherImages.heigth} src={"/images/FrameThree.png"} alt={""} />
-            }
+          </section>
+          <div className={classes.serviceCardContainer}>
+            <ServiceCards />
           </div>
-        </section>
-        <section className={classes.fourSectionContainer}>
-          <div className={`${classes.imageContainer} ${classes.otherImage}`}>
-            {!pantallaPequeña &&
-              <Image width={otherImages.width} height={720} src={"/images/FrameFour.png"} alt={""} />
-            }
-          </div>
-        </section>
-        <section className={classes.fiveSectionContainer}>
-          <div className={`${classes.imageContainer} ${classes.otherImage}`}>
-            {!pantallaPequeña &&
-              <Image width={otherImages.width + 2} height={720} src={"/images/FrameFive.png"} alt={""} />
-            }
-          </div>
-        </section>
-        <section className={classes.sixSectionContainers}>
-          <div className={`${classes.imageContainer} ${classes.otherImage}`}>
-            {!pantallaPequeña &&
-              <Image width={otherImages.width} height={720} src={"/images/FrameSix.png"} alt={""} />
-            }
-          </div>
-        </section>
-        <section className={classes.SevenSectionContainer}>
-          <div className={`${classes.imageContainer} ${classes.otherImage}`}>
-            {!pantallaPequeña &&
-              <Image width={firtAndLastImage.width} height={firtAndLastImage.heigth} src={"/images/FrameSevenInside.png"} alt={""} />
-            }
-          </div>
-        </section>
-        {/* <ServiceCards />
+          <section className={classes.secondSectionContainer}>
+            <div className={`${classes.imageContainer} ${classes.secondImage}`}>
+              <div className={classes.containerAbsolute}>
+                <div className={classes.aboutCardContainer}>
+                  <AboutUsCard />
+                  <div className={classes.aboutUsParagraphContainer}>
+                    <h1>ABOUT US</h1>
+                    <p>All Bencen, we are more than builders, we are creators of spaces that imspire.
+                      Our journey in the realm of construction has been fueled by a passion for excellence.
+                    </p>
+                    <p>
+                      From the drawing board to the final brick, we meticulously craft every project to reflect
+                      innovation, quality, and client aspirations. Join us in shaping a world where vision meets
+                      reality.
+                    </p>
+                    <button className={classes.aboutUsButton}>DISCOVER MORE ABOUT US</button>
+                  </div>
+                </div>
+              </div>
+              {!pantallaPequeña &&
+                <Image width={otherImages.width} height={otherImages.heigth} src={"/images/FrameTwoInside.png"} alt={""} />
+              }
+            </div>
+          </section>
+          <section className={classes.threeSectionContainer}>
+            <div className={`${classes.imageContainer} ${classes.otherImage}`}>
+              <div className={classes.containerAbsolute}>
+                <div className={classes.unmatchedServicesContainer}>
+                  <h1>UNMATCHED SERVICES</h1>
+                  <button>GET FREE QUOTATION</button>
+                </div>
+                <UnmatchedCards />
+              </div>
+              {!pantallaPequeña &&
+                <Image width={otherImages.width} height={otherImages.heigth} src={"/images/FrameThree.png"} alt={""} />
+              }
+            </div>
+          </section>
+          <section className={classes.fourSectionContainer}>
+            <div className={`${classes.imageContainer} ${classes.otherImage}`}>
+              {!pantallaPequeña &&
+                <Image width={otherImages.width} height={720} src={"/images/FrameFour.png"} alt={""} />
+              }
+            </div>
+          </section>
+          <section className={classes.fiveSectionContainer}>
+            <div className={`${classes.imageContainer} ${classes.otherImage}`}>
+              {!pantallaPequeña &&
+                <Image width={otherImages.width + 2} height={720} src={"/images/FrameFive.png"} alt={""} />
+              }
+            </div>
+          </section>
+          <section className={classes.sixSectionContainers}>
+            <div className={`${classes.imageContainer} ${classes.otherImage}`}>
+              {!pantallaPequeña &&
+                <Image width={otherImages.width} height={720} src={"/images/FrameSix.png"} alt={""} />
+              }
+            </div>
+          </section>
+          <section className={classes.SevenSectionContainer}>
+            <div className={`${classes.imageContainer} ${classes.otherImage}`}>
+              {!pantallaPequeña &&
+                <Image width={firtAndLastImage.width} height={firtAndLastImage.heigth} src={"/images/FrameSevenInside.png"} alt={""} />
+              }
+            </div>
+          </section>
+          {/* <ServiceCards />
       <UnmatchedCards /> */}
-      </div>
+        </div>
+      </div >
     </>
   );
 };
