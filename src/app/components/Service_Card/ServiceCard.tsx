9dc6a225118d627651781a1e1da28d0cbdf62f1b
ipiccard <ivan.piccardo@tsoftlatam.com>
2023-11-20 12:11:01 -0300

    Se ocultan las img en resoluciones menores a 1400px

--- src/app/components/Service_Card/ServiceCard.tsx
+++ src/app/components/Service_Card/ServiceCard.tsx
@@ -1,10 +1,14 @@
 'use client'
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import classes from './serviceCard.module.css';
 import Button from '../Ui/Button';
 import { SERVICES } from '../../utils/constants';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 interface Props {
   imageName: string;
@@ -16,42 +20,72 @@ const ServiceCard = ({ imageName, header, content }: Props): JSX.Element => {
   const [hover, setHover] = useState(false);
 
   return (
-    <div 
-    className={`${classes.serviceCard} ${hover ? classes.hover : ""}`}
-    onMouseEnter={() => setHover(true)}
-    onMouseLeave={() => setHover(false)}
+    <div
+      className={`${classes.serviceCard} ${hover ? classes.hover : ""}`}
+    // onMouseEnter={() => setHover(true)}
+    // onMouseLeave={() => setHover(false)}
     >
       <div className={classes.group}>
-        <Image 
-          className={classes.image} 
-          alt={`${header}`.toLowerCase() + " icon"} 
-          src={`/icons/Services/${hover ? imageName + "_hover" : imageName}.png`} 
-          width={96} 
-          height={96} 
+        <Image
+          className={classes.image}
+          alt={`${header}`.toLowerCase() + " icon"}
+          src={`/icons/Services/${hover ? imageName + "_hover" : imageName}.png`}
+          width={96}
+          height={96}
         />
         <div className={classes.text}>
           <div className={classes.header}>{header}</div>
           <div className={classes.content}>{content}</div>
         </div>
-        <Button href="" classNameButton={classes.button} text="FIND OUT MORE"/>
+        <Button href="" classNameButton={classes.button} text="FIND OUT MORE" />
       </div>
     </div>
   );
 };
 
 const ServiceCards = (): JSX.Element => {
+
+  const isMobile = typeof window !== 'undefined' && window.innerWidth < 1000;
+
+  // Configuración del slider
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: isMobile ? 1 : 4,
+    slidesToScroll: 1,
+  };
+
+
   return (
-    <ul className={classes.serviceCards}>
-    {Object.keys(SERVICES).map((key) => {
-      const [imageName, header, content] = SERVICES[key];
-      return (
-       <li key={key}>
-         <ServiceCard imageName={imageName} header={header} content={content} />
-       </li>
-       );
-    })}
-   </ul>
-  ) 
+    <div>
+      {isMobile ? (
+        <div className={classes.sliderContainer}>
+          <Slider {...sliderSettings}>
+            {Object.keys(SERVICES).map((key) => {
+              const [imageName, header, content] = SERVICES[key];
+              return (
+                <div key={key}>
+                  <ServiceCard imageName={imageName} header={header} content={content} />
+                </div>
+              );
+            })}
+          </Slider>
+        </div>
+      ) : (
+        <ul className={classes.serviceCards}>
+          {Object.keys(SERVICES).map((key) => {
+            const [imageName, header, content] = SERVICES[key];
+            return (
+              <li key={key}>
+                <ServiceCard imageName={imageName} header={header} content={content} />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  )
 };
 
 export default ServiceCards;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,11 +5,9 @@ import React, { useEffect, useState } from "react";
 import "../styles/globals.css";
 import ServiceCards from "../components/Service_Card/ServiceCard";
 import UnmatchedCards from "../components/Unmatched_Card/UnmatchedCard";
-import Hero from "../components/Hero/Hero";
 import classes from './page.module.css'
 import Header from "../components/Header/Header";
 import Image from "next/image";
-import Button from "../components/Ui/Button";
 import AboutUsCard from "../components/AboutUs_Card/AboutUsCard";
 import ClientCard from "../components/Clients_Card/ClientCard";
 import NewsCard from "../components/News_Card/NewsCard";
@@ -17,29 +15,22 @@ import NewsCard from "../components/News_Card/NewsCard";
 
 const HomePage = () => {
 
-  const [pantallaPequeña, setPantallaPequeña] = useState(false);
   const [pantallaMediana, setPantallaMediana] = useState(false);
   const [firtAndLastImage, setFirstAndLastImage] = useState({ width: 1344, heigth: 888 })
   const [otherImages, setOtherImages] = useState({ width: 1296, heigth: 960 })
 
   const handleResize = () => {
-    setPantallaPequeña(window.innerWidth < 768)
+    setPantallaMediana(window.innerWidth < 768)
     const screenWidth = window.innerWidth;
-    console.log(screenWidth)
 
-    if (screenWidth >= 1400) {
-      setPantallaPequeña(false);
+    if (screenWidth > 1400) {
       setPantallaMediana(false)
       setFirstAndLastImage({ width: 1344, heigth: 888 });
       setOtherImages({ width: 1344, heigth: 960 })
-    } else if (screenWidth >= 941) {
-      setPantallaPequeña(false);
+    } else {
       setPantallaMediana(true)
       setFirstAndLastImage({ width: 938, heigth: 888 });
       setOtherImages({ width: 938, heigth: 960 })
-    } else {
-      setPantallaPequeña(true);
-      setPantallaMediana(false)
     }
   };
   useEffect(() => {
@@ -52,12 +43,9 @@ const HomePage = () => {
     };
   }, []);
 
-
-
-
   return (
     <>
-      {/* <NewsCard /> */}
+
       <div className={classes.homePageContainer}>
         <div className={classes.headerContainer}>
           <Header />
@@ -66,9 +54,11 @@ const HomePage = () => {
           <section>
             <div className={`${classes.FirstimageContainer} ${classes.widthImage}`}>
               {!pantallaMediana ?
-                <Image width={firtAndLastImage.width} height={firtAndLastImage.heigth} src={"/images/withoutMark.png"} alt={""} />
+                <div className={classes.show}>
+                  <Image width={firtAndLastImage.width} height={firtAndLastImage.heigth} src={"/images/withoutMark.png"} alt={""} />
+                </div>
                 :
-                <Image src={"/images/withoutMark.png"} alt={""} layout="fill" objectFit="cover" />
+                <></>
               }
             </div>
           </section>
@@ -95,49 +85,59 @@ const HomePage = () => {
                 </div>
               </div>
               {!pantallaMediana ?
-                <Image width={otherImages.width} height={otherImages.heigth} src={"/images/FrameTwoInside.png"} alt={""} />
+                <div className={classes.show}>
+                  <Image width={otherImages.width} height={otherImages.heigth} src={"/images/FrameTwoInside.png"} alt={""} />
+                </div>
                 :
-                <Image src={"/images/FrameTwoInside.png"} alt={""} layout="fill" objectFit="cover" />
+                <></>
+                // <Image src={"/images/FrameTwoInside.png"} alt={""} layout="fill" objectFit="cover" />
               }
 
             </div>
           </section>
           <section className={classes.threeSectionContainer}>
-            <div className={`${classes.imageContainer} ${classes.otherImage} ${pantallaMediana && classes.widthOutMarginTop} ${pantallaMediana && classes.widthOutMarginTop}`}>
+            <div className={`${classes.imageContainer} ${classes.otherImage} ${classes.paddingBottom} ${pantallaMediana && classes.widthOutMarginTop} ${pantallaMediana && classes.widthOutMarginTop}`}>
               <div className={classes.containerAbsolute}>
                 <div className={classes.unmatchedServicesContainer}>
-                  <h1>UNMATCHED SERVICES</h1>
-                  <button>GET FREE QUOTATION</button>
+                  <div className={classes.unmatchedServicesParagraphContainer}>
+                    <h1>UNMATCHED SERVICES</h1>
+                    <button>GET FREE QUOTATION</button>
+                  </div>
+                  <UnmatchedCards />
                 </div>
-                <UnmatchedCards />
               </div>
               {!pantallaMediana ?
-                <Image width={1346} height={960} src={"/images/FrameThree.png"} alt={""} />
+                <div className={classes.show}>
+                  <Image width={1346} height={960} src={"/images/FrameThree.png"} alt={""} />
+                </div>
                 :
-                <Image src={"/images/FrameThree.png"} alt={""} layout="fill" objectFit="cover" />
+                <></>
+                // <Image src={"/images/FrameThree.png"} alt={""} layout="fill" objectFit="cover" />
               }
             </div>
           </section>
           <section className={classes.fourSectionContainer}>
             <div className={`${classes.imageContainer} ${classes.otherImage}`}>
               <div className={classes.containerAbsolute}>
-
                 <div className={classes.clientCardContainer}>
                   <ClientCard />
-                  <div className={classes.aboutUsParagraphContainer}>
+                  <div className={classes.clientParagraphContainer}>
                     <h1>SATISFIED CLIENTS WE SERVED FOR YEARS</h1>
                     <p>Our commitment to excellence and unwavering dedication have enabled us to build lasting relationships with individuals and businesses alike. Our track record of delivering exceptional service has not only met but exceeded the expectations of our valued clients.
 
                     </p>
                     <p>Their satisfaction is a testament to our expertise, reliability, and a deep-rooted commitment to their success.</p>
-                    <button className={classes.aboutUsButton}>SEE ALL PROJECTS</button>
+                    <button className={classes.clientButton}>SEE ALL PROJECTS</button>
                   </div>
                 </div>
               </div>
               {!pantallaMediana ?
-                <Image width={otherImages.width} height={720} src={"/images/FrameFour.png"} alt={""} />
+                <div className={classes.show}>
+                  <Image width={otherImages.width} height={720} src={"/images/FrameFour.png"} alt={""} />
+                </div>
                 :
-                <Image src={"/images/FrameFour.png"} alt={""} layout="fill" objectFit="cover" />
+                <></>
+                // <Image src={"/images/FrameFour.png"} alt={""} layout="fill" objectFit="cover" />
               }
             </div>
           </section>
@@ -155,13 +155,16 @@ const HomePage = () => {
                 </div>
               </div>
               {!pantallaMediana ?
-                <Image width={otherImages.width + 2} height={720} src={"/images/FrameFive.png"} alt={""} />
+                <div className={classes.show} >
+                  <Image width={otherImages.width + 2} height={720} src={"/images/FrameFive.png"} alt={""} />
+                </div>
                 :
-                <Image src={"/images/FrameFive.png"} alt={""} layout="fill" objectFit="cover" />
+                <></>
+                // <Image src={"/images/FrameFive.png"} alt={""} layout="fill" objectFit="cover" />
               }
             </div>
           </section>
-          <section className={classes.sixSectionContainers}>
+          <section className={classes.sixSectionContainer}>
             <div className={`${classes.imageContainer} ${classes.otherImage}`}>
               <div className={classes.containerAbsolute}>
 
@@ -171,25 +174,17 @@ const HomePage = () => {
                 </div>
               </div>
               {!pantallaMediana ?
-                <Image width={otherImages.width} height={720} src={"/images/FrameSix.png"} alt={""} />
+                <div className={classes.show}>
+                  <Image width={otherImages.width} height={720} src={"/images/FrameSix.png"} alt={""} />
+                </div>
                 :
-                <Image src={"/images/FrameSix.png"} alt={""} layout="fill" objectFit="cover" />
+                <></>
+                // <Image src={"/images/FrameSix.png"} alt={""} layout="fill" objectFit="cover" />
               }
             </div>
           </section>
-          {/* <section className={classes.SevenSectionContainer}>
-            <div className={`${classes.imageContainer} ${classes.otherImage}`}>
-              {!pantallaPequeña ?
-                <Image width={firtAndLastImage.width} height={firtAndLastImage.heigth} src={"/images/FrameSevenInside.png"} alt={""} />
-                :
-                <Image src={"/images/FrameSevenInside.png"} alt={""} layout="fill" objectFit="cover" />
-              }
-            </div>
-          </section> */}
         </div>
       </div >
-      {/* <ServiceCards />
-      <UnmatchedCards /> */}
     </>
   );
 };
