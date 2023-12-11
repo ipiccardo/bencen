f0cd06fb1ce678939b9f3a5b97942ecef6d6dcf7
rodg22 <giudicerodrigo@hotmail.com>
2023-12-11 09:25:50 -0300

    fix: responsive desktop styles

--- src/app/dashboard/contact/page.tsx
+++ src/app/dashboard/contact/page.tsx
@@ -8,24 +8,27 @@ import classes from "./page.module.css";
 import Image from "next/image";
 
 const Contact = () => {
-  const [Resolution, setResolution] = useState({ width: 0, height: 0,});
+  const [Resolution, setResolution] = useState({ width: 0, height: 0 });
   const [widthPercentage, SetWidthPercentage] = useState(1);
   const [heightPercentage, SetHeightPercentage] = useState(1);
-  
+
   const handleResolution = () => {
     // Set dimensions based on a percentage of the screen width and height:
-    if (window.innerWidth >= 1440){
+    if (window.innerWidth >= 1440) {
       SetWidthPercentage(1);
+      setResolution({
+        width: 1440,
+        height: Math.floor(window.innerHeight * heightPercentage),
+      });
     } else {
-      SetHeightPercentage(1 - (1- window.innerWidth/1440)/2);
-    } 
-    
-    setResolution({ 
-      width: Math.floor(window.innerWidth * widthPercentage), 
-      height: Math.floor(window.innerHeight * heightPercentage)
-    });
+      SetHeightPercentage(1 - (1 - window.innerWidth / 1440) / 2);
+      setResolution({
+        width: Math.floor(window.innerWidth * widthPercentage),
+        height: Math.floor(window.innerHeight * heightPercentage),
+      });
+    }
   };
-  
+
   useEffect(() => {
     handleResolution();
     window.addEventListener("resize", handleResolution);
@@ -42,19 +45,21 @@ const Contact = () => {
           <div className={classes.text}>
             <ConnectWithUs />
           </div>
-          <div className={classes.contactCard} style={{ width: Resolution.width * 0.9 }}>
-            <ContactCards />
-          </div>
           <Image
             width={Resolution.width > 900 ? Resolution.width : 900}
             height={Resolution.width > 900 ? Resolution.height : 756}
-            src={`/images/backgrounds/contact/${window.innerWidth > 900 ? '1' : '1-R'}.png`}
+            src={`/images/backgrounds/contact/${
+              window.innerWidth > 900 ? "1" : "1-R"
+            }.png`}
             alt={""}
           />
-          <div className={classes.preTab} style={{ width: Resolution.width * 0.9 }}></div>
+          <div className={classes.preTab}></div>
+          <div className={classes.contactCard}>
+            <ContactCards />
+          </div>
         </div>
         <div className={classes.secondSection}>
-          <section className={classes.tab} style={{ width: Resolution.width * 0.9 }}>
+          <section className={classes.tab}>
             <TabBar />
           </section>
         </div>
