8ebab40c31b8ebf40f4ddd48401e8ba768fe87aa
rodg22 <giudicerodrigo@hotmail.com>
2024-02-19 08:15:53 -0300

    fix: responsive contact

--- src/app/components/Contact_Card/ContactCard.tsx
+++ src/app/components/Contact_Card/ContactCard.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
 import React, { useEffect, useState, useContext } from "react";
 import Image from "next/image";
-import classes from './contactCard.module.css';
-import { CONTACT_CARD } from '../../utils/constants';
+import classes from "./contactCard.module.css";
+import { CONTACT_CARD } from "../../utils/constants";
 import Icon from "../Icon/Icon";
-import { store } from '@/app/context/context';
+import { store } from "@/app/context/context";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -23,23 +23,29 @@ const ContactCard = ({ imageName, header, content }: Props): JSX.Element => {
     if (window.innerWidth >= 1440) {
       setWidth(280);
     } else {
-      setWidth(240);
+      setWidth(255);
     }
   }, [window.innerWidth]);
 
   return (
-    <div className={classes.contactCard} style={{ width: `${width}px`, height: `${width}px`}}>
+    <div
+      className={classes.contactCard}
+      style={{ width: `${width}px`, height: `${width}px` }}
+    >
       <div className={classes.group}>
         <div className={classes.imageGroup}>
           <Image
-              className={classes.image} 
-              alt={`${header}`.toLowerCase()}
-              src={`/icons/Contact/${imageName}.svg`}
-              width={96} 
-              height={96}
+            className={classes.image}
+            alt={`${header}`.toLowerCase()}
+            src={`/icons/Contact/${imageName}.svg`}
+            width={96}
+            height={96}
           />
         </div>
-        <div className={classes.text} style={{width: `${width * 13/15}px`}}>
+        <div
+          className={classes.text}
+          style={{ width: `${(width * 13) / 15}px` }}
+        >
           <div className={classes.header}>{header}</div>
           {content ? (
             <pre className={classes.content}>{content}</pre>
@@ -78,13 +84,17 @@ const ContactCardSlider = (): JSX.Element => {
         const [imageName, header, content] = CONTACT_CARD[language][key];
         return (
           <div key={key}>
-            <ContactCard imageName={imageName} header={header} content={content} />
+            <ContactCard
+              imageName={imageName}
+              header={header}
+              content={content}
+            />
           </div>
         );
       })}
     </Slider>
   );
-}
+};
 
 const ContactCards = (): JSX.Element => {
   const { language }: any = useContext(store);
@@ -94,7 +104,11 @@ const ContactCards = (): JSX.Element => {
       const [imageName, header, content] = CONTACT_CARD[language][key];
       return (
         <li key={key}>
-          <ContactCard imageName={imageName} header={header} content={content} />
+          <ContactCard
+            imageName={imageName}
+            header={header}
+            content={content}
+          />
         </li>
       );
     });
@@ -102,20 +116,17 @@ const ContactCards = (): JSX.Element => {
 
   return (
     <>
-      {window.innerWidth > 1100 ? 
-        (
-          <ul className={classes.contactCards}>
-            {renderContactCards()}
-          </ul>
-        ) : (
-          <div className={`${classes.showSlider} serviceCardSliderContainer`}>
-            <div className={classes.sliderContainer}>
-              <ContactCardSlider/>
-            </div>
+      {window.innerWidth > 1100 ? (
+        <ul className={classes.contactCards}>{renderContactCards()}</ul>
+      ) : (
+        <div className={`${classes.showSlider} serviceCardSliderContainer`}>
+          <div className={classes.sliderContainer}>
+            <ContactCardSlider />
           </div>
-        )}
+        </div>
+      )}
     </>
-  ) 
+  );
 };
 
-export default ContactCards
+export default ContactCards;
