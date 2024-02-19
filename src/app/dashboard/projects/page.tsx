161d4f5ff7f4efd9cdc1f8a98948962f65e66f48
rodg22 <giudicerodrigo@hotmail.com>
2024-02-19 08:20:38 -0300

    .

--- src/app/dashboard/projects/page.tsx
+++ src/app/dashboard/projects/page.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import React from "react";
 import ExploreOurPortfolio from "../../components/Projects_Hero/ExploreOurPortfolio";
 import ProjectsSlidersContainer from "@/app/components/Projects_Sliders/ProjectsSlidersContainer";
 import FirstSection from "@/app/components/FirstSection/FirstSection";
