161a169609e3039017f346622c479a2ecb301e80
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-04-24 10:57:48 -0300

    Se modifican imagenes

--- src/app/components/FirstSection/FirstSection.tsx
+++ src/app/components/FirstSection/FirstSection.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import classes from "./firstSection.module.css";
 import Image from "next/image";
 
@@ -8,11 +10,6 @@ type FirstSectionProps = {
   secondChild?: JSX.Element;
   textToTheRight?: boolean;
   moreTranslate?: boolean;
-  widthInPx?: any;
-
-
-
-
 };
 
 const FirstSection = ({
@@ -21,11 +18,32 @@ const FirstSection = ({
   secondChild,
   textToTheRight = false,
   moreTranslate = false,
-  widthInPx,
 }: FirstSectionProps) => {
+  // const [Resolution, setResolution] = useState({ width: 0, height: 0 });
+  const [widthPercentage, SetWidthPercentage] = useState(1);
+  const [widthInPx, SetWidthInPx] = useState(1);
+  const [heightPercentage, SetHeightPercentage] = useState(1);
 
+  const handleResolution = () => {
+    // Set dimensions based on a percentage of the screen width and height:
+    if (window.innerWidth >= 1440) {
+      SetWidthPercentage(1);
+      SetWidthInPx(window.innerWidth);
+    } else {
+      SetHeightPercentage(1 - (1 - window.innerWidth / 1440) / 2);
+    }
+  };
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      handleResolution();
+      window.addEventListener("resize", handleResolution);
 
+      return () => {
+        window.removeEventListener("resize", handleResolution);
+      };
+    }
+  }, []);
 
   return (
     <div className={classes.firstSectionContainer}>
