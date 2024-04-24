bc6abff68d608a5e8209e27549dec139015f6961
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-04-24 15:27:22 -0300

    Se agregan imagenes de clientes en la home

--- src/app/components/Clients_Card/ClientCard.tsx
+++ src/app/components/Clients_Card/ClientCard.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import classes from './clientCard.module.css';
 import { store } from '@/app/context/context';
-import {PROJECTS_CARD} from '../../utils/constants';
+import { PROJECTS_CARD } from '../../utils/constants';
+import Image from 'next/image';
 
 const ClientCard = () => {
     const { language }: any = useContext(store)
@@ -12,6 +13,7 @@ const ClientCard = () => {
         <div className={classes.clientCardsContainer}>
             <div className={classes.clientCardContainer}>
                 <div className={classes.imageContainer}>
+                    <Image src='/images/projects/integrated_services/i5.jpg' fill alt={'sevicio'} />
                 </div>
                 <div className={classes.paragraphContainer}>
                     <h1>{PROJECTS_CARD[language][0]}</h1>
@@ -20,6 +22,7 @@ const ClientCard = () => {
             </div>
             <div className={classes.clientCardContainer}>
                 <div className={classes.imageContainer}>
+                    <Image src='/images/projects/integrated_services/i1.jpg' fill alt={'sevicio'} />
                 </div>
                 <div className={classes.paragraphContainer}>
                     <h1>{PROJECTS_CARD[language][0]}</h1>
@@ -28,6 +31,7 @@ const ClientCard = () => {
             </div>
             <div className={classes.clientCardContainer}>
                 <div className={classes.imageContainer}>
+                    <Image src='/images/projects/integrated_services/i6.jpg' fill alt={'sevicio'} />
                 </div>
                 <div className={classes.paragraphContainer}>
                     <h1>{PROJECTS_CARD[language][0]}</h1>
